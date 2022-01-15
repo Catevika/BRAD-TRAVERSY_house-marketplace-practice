a25600d6265d9d981aaf4a23ebe7cac168820de3
Catevika <dominique.bello@outlook.fr>
2022-01-15 19:12:19 +0100

    Add Google sign-in option

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -8,6 +8,7 @@ import {
 } from 'firebase/auth';
 import { db } from '../firebase.config';
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
+import OAuth from '../components/OAuth';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -116,7 +117,7 @@ function SignUp() {
 							</button>
 						</div>
 					</form>
-					{/* Google OAuth */}
+					<OAuth />
 					<Link to='/sign-in' className='registerLink'>
 						Sign In Instead
 					</Link>
